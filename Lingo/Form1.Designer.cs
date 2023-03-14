f75f71d0c6959a76cb05e81a16a96b1517fb79ee
apire <duboisandre092@gmail.com>
2023-03-14 21:00:03 +0100

    lingo

---
+++ Lingo/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Lingo
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainButton = new System.Windows.Forms.Button();
+            this.inputTxtBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // mainButton
+            // 
+            this.mainButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.mainButton.Location = new System.Drawing.Point(0, 256);
+            this.mainButton.Name = "mainButton";
+            this.mainButton.Size = new System.Drawing.Size(614, 55);
+            this.mainButton.TabIndex = 0;
+            this.mainButton.Text = "START";
+            this.mainButton.UseVisualStyleBackColor = true;
+            this.mainButton.Click += new System.EventHandler(this.mainButton_Click);
+            // 
+            // inputTxtBox
+            // 
+            this.inputTxtBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.inputTxtBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTxtBox.Location = new System.Drawing.Point(0, 0);
+            this.inputTxtBox.Name = "inputTxtBox";
+            this.inputTxtBox.Size = new System.Drawing.Size(614, 29);
+            this.inputTxtBox.TabIndex = 1;
+            this.inputTxtBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputTxtBox_KeyDown);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(0, 29);
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.Size = new System.Drawing.Size(614, 227);
+            this.outputTextBox.TabIndex = 2;
+            this.outputTextBox.Text = "";
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 311);
+            this.Controls.Add(this.outputTextBox);
+            this.Controls.Add(this.inputTxtBox);
+            this.Controls.Add(this.mainButton);
+            this.Name = "mainForm";
+            this.ShowIcon = false;
+            this.Text = "Lingo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.TextBox inputTxtBox;
+        private System.Windows.Forms.RichTextBox outputTextBox;
+    }
+}
+
---
+++ Lingo/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Lingo
+{
+    public partial class mainForm : Form
+    {
+        bool isPlaying = false;
+        string gekozenWoord = string.Empty;
+        int pogingen = 0;
+        List<char> gekozenLetters = new List<char>();
+
+        static string[] woorden = new string[]
+{
+        "appel",
+        "banaan",
+        "kat",
+        "hond",
+        "olifant",
+        "bloem",
+        "gitaar",
+        "huis",
+        "ijs",
+        "jas",
+        "kangoeroe",
+        "citroen",
+        "aap",
+        "bal",
+        "boot",
+        "dans",
+        "fiets",
+        "klok",
+        "koe",
+        "lamp",
+        "neus",
+        "oor",
+        "potlood",
+        "stoel",
+        "tijger",
+        "vogel",
+        "zee",
+        "zwaan",
+        "boom",
+        "duim",
+        "ei",
+        "fles",
+        "goud",
+        "haar",
+        "ijsbeer",
+        "judo",
+        "kaas",
+        "leeuw",
+        "muis",
+        "noot",
+        "olie",
+        "paard",
+        "regen",
+        "schaap",
+        "toren",
+        "uier",
+        "verf",
+        "winkel",
+        "xylofoon",
+        "yoghurt",
+        "zeep",
+        "appeltaart",
+        "brood",
+        "chocolade",
+        "diamant",
+        "envelop",
+        "friet",
+        "groen",
+        "haai",
+        "ijsje",
+        "jacht",
+        "kruis",
+        "laser",
+        "molen",
+        "nacht",
+        "olijf",
+        "pinda",
+        "quiz",
+        "rijst",
+        "schelp",
+        "toeter",
+        "ui",
+        "vlag",
+        "wortel",
+        "xylofoon",
+        "yacht",
+        "zeester",
+        "aardbei",
+        "bloemkool",
+        "cavia",
+        "dennenboom",
+        "egel",
+        "fazant",
+        "giraffe",
+        "hamer",
+        "ijsberg",
+        "judo",
+        "kast",
+        "luipaard",
+        "muur",
+        "nijlpaard",
+        "olifantenpoot",
+        "pauw",
+        "quizzen",
+        "raket",
+        "spin",
+        "tegel",
+        "uil",
+        "vleugel",
+        "wafel",
+        "xylofoons",
+        "yoghurtijs",
+        "zebraprint",
+        "aapje",
+        "berg",
+        "champignon",
+        "dak",
+        "elektriciteit",
+        "flamingo",
+        "gazon",
+        "hamburger",
+        "ijsvogel",
+        "jaguar",
+        "kreeft",
+        "lam",
+        "marmot",
+        "narcis",
+        "olijfolie",
+        "papegaai"
+
+        };
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainButton_Click(object sender, EventArgs e)
+        {
+            if (isPlaying)
+            {
+                isPlaying = false;
+                mainButton.Text = "START";
+                return;
+            }
+            if (!isPlaying)
+            {
+                isPlaying = true;
+                mainButton.Text = "STOP";
+                GameStart();
+            }
+        }
+        private void GameStart()
+        {
+            Random r = new Random();
+
+            // Reset
+            outputTextBox.Clear();
+            pogingen = 0;
+            gekozenLetters.Clear();
+
+            gekozenWoord = woorden[r.Next(0, woorden.Length)];
+            outputTextBox.Text += gekozenWoord[0] + " ";
+            gekozenLetters.Add(gekozenWoord[0]);
+            for (int i = 1; i < gekozenWoord.Length; i++)
+            {
+                gekozenLetters.Add('_');
+                outputTextBox.Text += gekozenLetters[i] + " ";
+            }
+            outputTextBox.Text += '\n';
+        }
+
+        private void inputTxtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Enter)
+            {
+                if(pogingen == 5)
+                {
+                    MessageBox.Show("Je hebt verloren! Het woord was " + gekozenWoord, "Verloren", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    isPlaying = false;
+                    mainButton.Text = "START";
+                }
+                if (!isPlaying) return;
+
+                pogingen++;
+                string ingevoerdWoord = inputTxtBox.Text.ToLower().Trim(' ');
+
+                if (ingevoerdWoord.Length != gekozenWoord.Length)
+                {
+                    MessageBox.Show("Gelieve hetzelfde een woord door te geven met hetzelfde aantal karakters", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (gekozenWoord == ingevoerdWoord)
+                {
+                    MessageBox.Show("Je hebt het hele woord geraden in " + pogingen + " aantal poging(en)", "Gewonnen!", MessageBoxButtons.OK);
+                    isPlaying = false;
+                    mainButton.Text = "START";
+                }
+
+                for(int i = 0; i < ingevoerdWoord.Length; i++)
+                {
+                    if (ingevoerdWoord[i] == gekozenWoord[i])
+                    {
+                        gekozenLetters[i] = ingevoerdWoord[i];
+                    }
+                }
+
+                for (int i = 0; i < ingevoerdWoord.Length; i++)
+                {
+                    if (gekozenWoord.Contains(ingevoerdWoord[i]) && !gekozenLetters.Contains(ingevoerdWoord[i]))
+                    {
+                        outputTextBox.Text += "\nHet woord bezit ergens de letter '" + ingevoerdWoord[i] + "' maar niet op die plek! \n";
+                    }
+                }
+
+
+                for(int i = 0; i < gekozenLetters.Count;i++)
+                {
+                    outputTextBox.Text += gekozenLetters[i] + " ";
+                }
+
+                outputTextBox.Text += "\n";
+
+                inputTxtBox.Clear();
+            }
+        }
+    }
+}
